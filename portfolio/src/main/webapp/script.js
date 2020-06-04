ef8c6d2c50ba1b74f8566fbe2270bc9a217d89d8
g-lilian <guolilian98@gmail.com>
2020-06-05 01:19:14 +0800

    Prevent same quote from showing consecutively

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,5 @@
+var quoteNum = 0;
+
 /**
  * Adds a random quote to the page.
  */
@@ -6,10 +8,19 @@ function addRandomQuote() {
       ['“Art washes away from the soul the dust of everyday life.” --Pablo Picasso',
        '“Fiction, because it is not about somebody who actually lived in the real world, always has the possibility of being about oneself.” --Orson Scott Card',
        '“Somewhere, something incredible is waiting to be known.” --Carl Sagan',
-       '“Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.” --Albert Einstein'];
+       '“Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.” --Albert Einstein',
+       '“Any sufficiently advanced technology is indistinguishable from magic.” --Arthur C. Clarke'];
 
   // Pick a random quote.
-  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  var newQuoteNum = Math.floor(Math.random() * quotes.length);
+  
+  // Prevent the same quote from showing consecutively
+  if (newQuoteNum == quoteNum) {
+      ++newQuoteNum;
+      newQuoteNum %= quotes.length;
+  }
+  quoteNum = newQuoteNum;
+  const quote = quotes[quoteNum];
 
   // Add it to the page.
   const quoteContainer = document.getElementById('quote-container');
