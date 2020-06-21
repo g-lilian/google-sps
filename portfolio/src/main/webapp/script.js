a6e00da61f59bb539e370c7600eeb4e09b3313c0
g-lilian <guolilian98@gmail.com>
2020-06-21 21:07:14 +0800

    Create modals for gallery

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -95,3 +95,25 @@ async function drawChart() {
       document.getElementById('chart-container'));
   chart.draw(data, options);
 }
+
+/** Display gallery modal image. */
+function displayModal(img_id) {
+  // Get the modal
+  var modal = document.getElementById("modal-id");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById(img_id);
+  var modalImg = document.getElementById("modal-img");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+}
